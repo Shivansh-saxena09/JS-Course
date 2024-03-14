5d67da3da606aaed2c7079ce0370bd5697510810
Shivansh Saxena <saxenashivansh230@gmail.com>
2024-03-14 15:59:54 +0530

    Data Types

---
+++ 1_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(5 + 5)  // we are using nodejs not browser
+
+
+// number
+// bigint
+//string=> ""
+// boolean => true/false
+// null => standalone value;
+// undefined
+// symbol =>  unique
+
+
+// Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
