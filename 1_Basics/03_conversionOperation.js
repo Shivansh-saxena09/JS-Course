2690204f1f8f9db02be42a4740fb2f0e39aa5d3b
Shivansh Saxena <saxenashivansh230@gmail.com>
2024-03-14 16:23:52 +0530

    Conversion of Data Types

---
+++ 1_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "56";
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber))
+
+// "33" => 33
+// "33fgh" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "shivansh"
+
+let booleanIsLoggedIn =  Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn)
+
+ //1 => true; 0 => false
+ // '' => false
+ // "shivansh" => true
+
+ let someNumber = 67;
+  let stringNumber = String(someNumber)
+  console.log(typeof(stringNumber));
