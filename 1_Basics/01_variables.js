d68cd826256cf84266eacadc8e2947e9e682508c
Shivansh Saxena <saxenashivansh230@gmail.com>
2024-03-14 15:19:40 +0530

    understand Variables in JS

---
+++ 1_Basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 7688;
+
+let userEmail = "shivansh@gmailcom";
+
+let userState;
+
+var userPassword = "8900";
+
+userCity = "New Delhi";
+
+// accountId = 345;  // not allowed
+
+userEmail="shiv@yahoo.com"
+
+userPassword="1234";
+
+userCity="noida"
+
+/*
+ Kosis Karo ki Var use na kro  jiski wajha hai block scope and functional scope
+ 
+*/
+
+
+
+
+console.table([accountId,userEmail,userPassword,userCity,userState]);
