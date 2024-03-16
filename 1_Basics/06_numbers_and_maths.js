4fc7a271c5af81da29177a5cf1972aeb0d2025ad
Shivansh Saxena <saxenashivansh230@gmail.com>
2024-03-16 19:57:58 +0530

    Math and Number

---
+++ 1_Basics/06_numbers_and_maths.js
@@ -0,0 +1,39 @@
+const loan = 2500;
+// console.log(loan);
+
+const earning = new Number(60000);
+// console.log(earning);
+
+
+// console.log(earning.toString().length);
+
+// console.log(earning.toFixed(2));
+
+
+const neNum = 4565.8;
+
+// console.log(neNum.toPrecision(3))
+
+
+const mathsmatical = 100000 ;
+// console.log(mathsmatical.toLocaleString('en-IN'));
+
+
+// ####################### Maths ###########################################################
+//  console.log(Math);
+
+//  console.log(Math.abs(-8));
+//  console.log(Math.round(4.5));
+//  console.log(Math.ceil(4.1));
+//  console.log(Math.floor(4.9));
+//  console.log(Math.sqrt(25));
+//  console.log(Math.min(5,8,3,10,34,56,78));
+
+
+ console.log(Math.random()) //hamesha 0 or 1 k beech me he ayegi value 
+
+ console.log((Math.random()*10) +1)
+
+ const minNumber =20;
+ const maxNumber = 30;
+ console.log(Math.floor(Math.random()*(maxNumber - minNumber +1))+minNumber)
