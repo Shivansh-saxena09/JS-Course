8bb62e7f9d078f53d43d02f3b0d31d864262ef36
Shivansh-saxena09 <saxenashivansh230@gmail.com>
2024-04-05 15:44:59 +0530

    Objects in JS part 2

---
+++ 2_Basics/04_objects2.js
@@ -0,0 +1,54 @@
+//const fbUser=new Object() // this is a singleton object.
+
+const fbUser = {} // this is not a singleton object.
+
+fbUser.id = "243503"
+fbUser.name="Shiva"
+fbUser.isLoggedIn=true
+
+// console.log(fbUser)
+
+const membershipUser = {
+    email:"123@abc.com",
+    fullname:{
+        userfullname:{
+            firstname:"Shiva",
+            lastname:"saxena"
+        }
+    }
+}
+
+
+console.log(membershipUser.fullname)
+console.log(membershipUser.fullname.userfullname)
+
+
+const obj1 ={1:"Shiva",2:"Abhi",3:"Simran",4:"Saloni"}
+const obj2 = {5:"Btech",6:"B.com",7:"BTC",8:"Bsc"}
+
+const obj3 ={obj1,obj2} // this is not teh way to merege objects.
+// console.log(obj3)
+
+const objcombine = Object.assign({},obj1,obj2); // this is the correct way to combine any objects.
+// console.log(objcombine)
+
+
+
+const objcombine2 ={...obj1,...obj2} // this is also a correct way to combine any object or we can say here we spread the objects.
+// console.log(objcombine2)
+
+const users = [
+    {
+        id:123,
+        email:"123@abc.com",
+    },
+]
+// users[1].email
+console.log(fbUser)
+
+console.log(Object.keys(fbUser)) // this will gives all the keys of an object in an array.
+console.log(Object.values(fbUser))
+console.log(Object.entries(fbUser)) // it will make all the keys with value in a array inside the array.
+console.log(fbUser.hasOwnProperty('isLoggedI')) // it will check is this property  is avialbake in your object or not .
+
+
