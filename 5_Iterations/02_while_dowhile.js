d234bc6ff576e653fb61d7cea579fd0d13274955
Shivansh-saxena09 <saxenashivansh230@gmail.com>
2024-04-10 07:56:05 +0530

    Whilw an dowhilw loop in JS

---
+++ 5_Iterations/02_while_dowhile.js
@@ -0,0 +1,25 @@
+// while loop 
+
+
+let i = 0
+while (i<=10) {
+    // console.log(`value of index is ${i}`)
+    i = i+3
+}
+
+
+let myArray=["Virat", "Rohit", "Raina"]
+let arr = 0
+while(arr<myArray.length){
+// console.log(`value is ${myArray[arr]}`)
+arr++
+}
+
+
+//dowhile loop
+
+ let marks =21
+do {
+    console.log(`marks is :${marks}`)
+    marks++
+} while (marks<20);
